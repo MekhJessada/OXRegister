9148c7c11af592bb4d073625b3826bfa0d6436e2
PopAps <23138@tkschool.ac.th>
2018-10-12 08:50:59 +0700

    first login

---
+++ src/login.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class login {
+    private JButton loginButton;
+    private JButton clearButton;
+    private JButton registerButton;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+}
